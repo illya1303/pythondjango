f39aca2e548eeb5b72f769766e90eb0a56a29eaf
illya1303 <nepodzhigaemiitankist777@gmail.com>
2024-10-15 13:58:17 +0200

    create models and migrations

--- shop/product/models.py
+++ shop/product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=9)
+    counter = models.PositiveIntegerField(default=0)
+    categories = models.ForeignKey(to=Categories, on_delete=models.CASCADE)
